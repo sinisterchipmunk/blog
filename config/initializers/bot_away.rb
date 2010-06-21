36df451a7bde91526a021cd30bf2908658cadbe6
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2010-06-21 07:32:35 -0400

    bot-away exceptions

--- config/initializers/bot_away.rb
+++ config/initializers/bot_away.rb
@@ -1,3 +1,3 @@
 #BotAway.show_honeypots = true
 #BotAway.dump_params = true
-ActionController::Request.accepts_unfiltered_params "role_ids"
+ActionController::Request.accepts_unfiltered_params "role_ids", "category_ids", "tag_ids", "name"
