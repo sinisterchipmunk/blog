000ae820e82a5b401fb1ac28ee284480c065f043
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2010-10-14 17:17:44 -0400

    Ping Google for reindexing when posts are updated or commented on

--- app/observers/comment_observer.rb
+++ app/observers/comment_observer.rb
@@ -3,4 +3,12 @@ class CommentObserver < ActiveRecord::Observer
   def after_create(comment)
     CommentMailer.deliver_new_comment(comment)
   end
+  
+  def after_save(comment)
+    # FIXME comment.post should respond_to blog, but it doesn't
+    Google::Blogsearch::Ping.new("Thoughts in Computation",
+                                 ActionController::Integration::Session.new.rss_feed_path,
+                                 :updated => ActionController::Integration::Session.new.post_path(comment.post))
+    Rails.logger.info "Pinged google."
+  end
 end
--- app/observers/post_observer.rb
+++ app/observers/post_observer.rb
@@ -1,4 +1,12 @@
 class PostObserver < ActiveRecord::Observer
   def after_create(post)
   end
+
+  def after_save(post)
+    # FIXME comment.post should respond_to blog, but it doesn't
+    Google::Blogsearch::Ping.new("Thoughts in Computation",
+                                 ActionController::Integration::Session.new.rss_feed_path,
+                                 :updated => ActionController::Integration::Session.new.post_path(post))
+    Rails.logger.info "Pinged google."
+  end
 end
