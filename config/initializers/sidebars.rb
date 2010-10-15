e82577e72612b4a8e41f9bee2532265a3ea28ea9
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2010-10-15 08:19:05 -0400

    Updated readme

--- config/initializers/sidebars.rb
+++ config/initializers/sidebars.rb
@@ -1,5 +1,7 @@
 # A list of sidebars in the order they will appear on the page. Each entry should have a corresponding View file located
-# in /app/views/sidebars/_#{sidebar_name}.html.erb
+# in /app/views/sidebars/_#{sidebar_name}.html.erb . Simply comment a sidebar out to toggle it off. They will appear in
+# the order listed below, so you can move them around if you want to change order of appearance.
+
 SIDEBARS = []
 SIDEBARS << "user_nav"
 SIDEBARS << "categories"
